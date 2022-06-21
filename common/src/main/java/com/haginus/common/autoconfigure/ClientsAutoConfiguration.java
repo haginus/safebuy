50be374e5582da3af73d006779d3b8c999cbc751
Haginus <hagiandrei.ah@gmail.com>
2022-06-21 21:40:36 +0300

    Add banners

--- common/src/main/java/com/haginus/common/autoconfigure/ClientsAutoConfiguration.java
+++ common/src/main/java/com/haginus/common/autoconfigure/ClientsAutoConfiguration.java
@@ -1,5 +1,6 @@
 package com.haginus.common.autoconfigure;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
@@ -8,8 +9,9 @@ import org.springframework.context.annotation.PropertySources;
 @PropertySources({
   @PropertySource("classpath:clients-${spring.profiles.active}.properties")
 })
+@Log4j2
 public class ClientsAutoConfiguration {
   ClientsAutoConfiguration() {
-
+    log.info("Clients auto configuration inserted.");
   }
 }
