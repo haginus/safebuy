3235542e5a04cf5775b1b333047c5231a52cacd7
Haginus <hagiandrei.ah@gmail.com>
2022-06-21 12:28:58 +0300

    Add hateoas

--- common/src/main/java/com/haginus/common/clients/marketplace/dto/ListingCategory/ListingCategoryResponseDto.java
+++ common/src/main/java/com/haginus/common/clients/marketplace/dto/ListingCategory/ListingCategoryResponseDto.java
@@ -1,13 +1,14 @@
 package com.haginus.common.clients.marketplace.dto.ListingCategory;
 
 import lombok.*;
+import org.springframework.hateoas.RepresentationModel;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class ListingCategoryResponseDto {
+public class ListingCategoryResponseDto extends RepresentationModel<ListingCategoryResponseDto> {
   private Long id;
   private String name;
   private String icon;
--- marketplace/src/main/java/com/haginus/marketplace/controller/ListingCategoryController.java
+++ marketplace/src/main/java/com/haginus/marketplace/controller/ListingCategoryController.java
@@ -6,8 +6,13 @@ import com.haginus.marketplace.mapper.ListingCategoryMapper;
 import com.haginus.marketplace.model.ListingCategory;
 import com.haginus.marketplace.service.ListingCategoryService;
 import lombok.AllArgsConstructor;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.Link;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 import javax.validation.Valid;
 import java.net.URI;
@@ -22,43 +27,99 @@ public class ListingCategoryController {
   private final ListingCategoryService listingCategoryService;
   private final ListingCategoryMapper listingCategoryMapper;
 
-  @GetMapping
-  public ResponseEntity<List<ListingCategoryResponseDto>> getAll() {
-    return ResponseEntity.ok().body(
-      this.listingCategoryService.getAll().stream()
-        .map(this.listingCategoryMapper::toDto)
-        .collect(Collectors.toList())
-    );
+  @GetMapping(produces = {"application/json"})
+  public ResponseEntity<List<ListingCategoryResponseDto>> getAllJson() {
+    List<ListingCategoryResponseDto> dtoList = this.listingCategoryService.getAll().stream()
+      .map(this.listingCategoryMapper::toDto)
+      .collect(Collectors.toList());
+    return ResponseEntity.ok().body(dtoList);
   }
 
-  @GetMapping("/{id}")
+  @GetMapping(produces = {"application/hal+json"})
+  public ResponseEntity<CollectionModel<ListingCategoryResponseDto>> getAll() {
+    List<ListingCategoryResponseDto> dtoList = this.listingCategoryService.getAll().stream()
+      .map(this.listingCategoryMapper::toDto)
+      .map(this::addLinks)
+      .collect(Collectors.toList());
+
+    Link link = linkTo(methodOn(ListingCategoryController.class).getAll()).withSelfRel();
+    CollectionModel<ListingCategoryResponseDto> collectionModel = CollectionModel.of(dtoList, link);
+    return ResponseEntity.ok().body(collectionModel);
+  }
+
+  @GetMapping(value = "/{id}", produces = "application/json")
+  public ResponseEntity<ListingCategoryResponseDto> getJson(@PathVariable Long id) {
+    ListingCategoryResponseDto dto =  this.listingCategoryMapper.toDto(this.listingCategoryService.get(id));
+    return ResponseEntity.ok().body(dto);
+  }
+
+  @GetMapping(value = "/{id}", produces = "application/hal+json")
   public ResponseEntity<ListingCategoryResponseDto> get(@PathVariable Long id) {
-    return ResponseEntity.ok().body(
-      this.listingCategoryMapper.toDto(this.listingCategoryService.get(id))
-    );
+    ListingCategoryResponseDto dto =  this.listingCategoryMapper.toDto(this.listingCategoryService.get(id));
+    this.addLinks(dto);
+    return ResponseEntity.ok().body(dto);
   }
 
-  @PostMapping
-  public ResponseEntity<ListingCategoryResponseDto> create(@Valid @RequestBody ListingCategoryRequestDto dto) {
+  private ListingCategoryResponseDto _create(ListingCategoryRequestDto dto) {
     ListingCategory listingCategory = this.listingCategoryMapper.toEntity(dto);
     ListingCategory result = this.listingCategoryService.create(listingCategory);
-    return ResponseEntity
-      .created(URI.create("/categories/" + result.getId()))
-      .body(this.listingCategoryMapper.toDto(result));
+    return this.listingCategoryMapper.toDto(result);
   }
 
-  @PutMapping("/{id}")
-  public ResponseEntity<ListingCategoryResponseDto> update(@PathVariable Long id, @Valid @RequestBody ListingCategoryRequestDto dto) {
+  @PostMapping(produces = "application/json")
+  public ResponseEntity<ListingCategoryResponseDto> createJson(@Valid @RequestBody ListingCategoryRequestDto dto) {
+    ListingCategoryResponseDto resultDto = _create(dto);
+    this.addLinks(resultDto);
+    URI locationUri = ServletUriComponentsBuilder.fromCurrentRequest()
+      .path("/{categoryId}").buildAndExpand(resultDto.getId())
+      .toUri();
+    return ResponseEntity.created(locationUri).body(resultDto);
+  }
+
+  @PostMapping(produces = "application/hal+json")
+  public ResponseEntity<ListingCategoryResponseDto> create(@Valid @RequestBody ListingCategoryRequestDto dto) {
+    ListingCategoryResponseDto resultDto = _create(dto);
+    this.addLinks(resultDto);
+    URI locationUri = ServletUriComponentsBuilder.fromCurrentRequest()
+      .path("/{categoryId}").buildAndExpand(resultDto.getId())
+      .toUri();
+    return ResponseEntity.created(locationUri).body(resultDto);
+  }
+
+  private ListingCategoryResponseDto _update(Long id, ListingCategoryRequestDto dto) {
     ListingCategory listingCategory = this.listingCategoryMapper.toEntity(dto);
     listingCategory.setId(id);
     ListingCategory result = this.listingCategoryService.update(listingCategory);
-    return ResponseEntity.ok().body(this.listingCategoryMapper.toDto(result));
+    return this.listingCategoryMapper.toDto(result);
+  }
+
+  @PutMapping(value = "/{id}", produces = "application/json")
+  public ResponseEntity<ListingCategoryResponseDto> updateJson(@PathVariable Long id,
+                                                           @Valid @RequestBody ListingCategoryRequestDto dto) {
+    ListingCategoryResponseDto resultDto = _update(id, dto);
+    return ResponseEntity.ok().body(resultDto);
+  }
+
+  @PutMapping(value = "/{id}", produces = "application/hal+json")
+  public ResponseEntity<ListingCategoryResponseDto> update(@PathVariable Long id,
+                                                           @Valid @RequestBody ListingCategoryRequestDto dto) {
+    ListingCategoryResponseDto resultDto = _update(id, dto);
+    this.addLinks(resultDto);
+    return ResponseEntity.ok().body(resultDto);
   }
 
   @DeleteMapping("/{id}")
   public ResponseEntity<?> delete(@PathVariable Long id) {
     this.listingCategoryService.delete(id);
     return ResponseEntity.noContent().build();
+  }
+
+  private ListingCategoryResponseDto addLinks(ListingCategoryResponseDto dto) {
+    Link selfLink = linkTo(methodOn(ListingCategoryController.class).get(dto.getId())).withSelfRel();
+    Link updateLink = linkTo(methodOn(ListingCategoryController.class).update(dto.getId(), null)).withRel("update");
+    Link deleteLink = linkTo(methodOn(ListingCategoryController.class).delete(dto.getId())).withRel("delete");
+    Link listLink = linkTo(methodOn(ListingCategoryController.class).getAll()).withRel("list");
+    return dto.add(selfLink, updateLink, deleteLink, listLink);
   }
 
 }
